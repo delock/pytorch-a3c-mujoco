c402e7b61a693adf8f79282d248f5dfcc228b7f0
Ma, Guokai <guokai.ma@gmail.com>
2022-02-14 08:59:03 +0800

    [pendulum weak] add weak version of inverted pendulum-down

--- env/__init__.py
+++ env/__init__.py
@@ -1,6 +1,7 @@
 import logging
 from gym.envs.registration import register
 from env.inverted_pendulum_down import InvertedPendulumDownEnv
+from env.inverted_pendulum_down_weak import InvertedPendulumDownWeakEnv
 
 logger = logging.getLogger(__name__)
 
@@ -9,4 +10,11 @@ register(
     entry_point="env:InvertedPendulumDownEnv",
     max_episode_steps=1000,
     reward_threshold=1000.0,
+)
+
+register(
+    id="InvertedPendulum-down-weak",
+    entry_point="env:InvertedPendulumDownWeakEnv",
+    max_episode_steps=5000,
+    reward_threshold=5000.0,
 )
