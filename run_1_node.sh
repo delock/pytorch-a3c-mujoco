ad89d0dd8e7ed45e103b1c09828d11236ba731ac
Ma, Guokai <guokai.ma@gmail.com>
2022-02-19 17:50:25 +0800

    [keypress] press any key to exit the program

--- run_1_node.sh
+++ run_1_node.sh
@@ -3,15 +3,14 @@ ENV_NAME=$1
 NUM_CPUS=`cat /proc/cpuinfo|grep core|grep id|sort -u|wc -l`
 
 WORLD_SIZE=$NUM_CPUS
-echo Start 1 optimizer worker and $WORLD_SIZE training worker
 WORLD_SIZE_MINUS_1=`expr $WORLD_SIZE - 1`
 
+echo Start 1 optimizer worker
 mkdir -p sync
 rm -f sync/*.pt
 numactl --physcpubind=0 python main.py --task optimize --save_freq 1000 --world_size $WORLD_SIZE --env-name $ENV_NAME &
 
-for core_id in `seq 0 $WORLD_SIZE_MINUS_1`
-do
-    numactl --physcpubind=$core_id python main.py --task train --save_freq 1000 --rank $core_id --display True --env-name $ENV_NAME &
-done
-wait
+./run_trainer.sh $ENV_NAME
+
+read -n1 ans
+pkill python
